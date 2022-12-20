23a447236b73d2738303da32328b0f20f0592818
Muhammed <muhammed1410@yandex.ru>
2022-12-20 18:19:21 +0300

    First exercise has been solved

--- L1/main.go
+++ L1/main.go
@@ -1,28 +1,44 @@
 package main
 
-import "fmt"
+//Human  - родительсякая структура
 
-type ProductList []Product
-
-func (products *ProductList) calcTotalCategory() map[string]float64 {
-	totals := make(map[string]float64)
-	for _, p := range *products {
-		totals[p.category] = totals[p.category] + p.price
-	}
-	return totals
+type Human struct {
+	name   string
+	age    int
+	sex    string
+	weight int
+	height int
 }
 
-func getProducts() []Product {
-	return []Product{
-		{"Kayak", "Watersports", 275},
-		{"Lifejacket", "Watersports", 48.95},
-		{"Soccer Ball", "Soccer", 19.50},
+// Конструктор для создания Human
+
+func NewHuman(name string, age int, sex string, weight, height int) *Human {
+	return &Human{name, age, sex, weight, height}
+}
+
+// Метод который реализует Human
+func (h *Human) isAdult() bool {
+	return h.age >= 18
+}
+
+// Action дочерняя структура Human которая наследует его (Human) поля и методы
+// Прикол Action в том что - эта структура во многом повторяет Human. Этой структуре придется описывать только доп. функциональность
+
+type Action struct {
+	*Human        // Встроенный тип
+	job    string // Регулярное поля
+	alone  bool   //Регулярное поле
+}
+
+// Функция NewAction — это конструктор, который использует свои
+//параметры для создания Action со встроенным значением Human
+
+func NewAction(name string, age int, sex string, weight int, height int, job string, alone bool) *Action {
+	return &Action{
+		NewHuman(name, age, sex, weight, height), job, alone,
 	}
 }
 
-func main() {
-	products := ProductList(getProducts())
-	for category, total := range products.calcTotalCategory() {
-		fmt.Println("Category: ", category, "Total: ", total)
-	}
+func (a *Action) isFat() bool {
+	return a.weight > 90
 }
--- L1/product.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type Product struct {
-	name, category string
-	price          float64
-}
