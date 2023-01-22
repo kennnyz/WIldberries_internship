ac1bef245aac3c4861f3cbf6ce38b6546df3e939
kennnyz <muhammed1410@yandex.ru>
2023-01-22 23:23:03 +0300

    p

--- L2/string_unpacker/stringUnpacker_test.go
+++ L2/string_unpacker/stringUnpacker_test.go
@@ -4,23 +4,31 @@ import "testing"
 
 func TestUnpack(t *testing.T) {
 	expected := "aaabcc"
-	if Unpack("a3bc2") != expected {
-		t.Errorf("Expected {%s} but got {%s} ", expected, Unpack("a3bc2"))
+	s, err := UnpackingString("aaabcc")
+	if s != expected && err != nil {
+		t.Errorf("Expected {%s} but got {%s} ", expected, s)
+	}
+	s, err = UnpackingString("")
+
+	if s != "" && err != nil {
+		t.Errorf("Expected {%s} but got {%s} ", expected, s)
 	}
 
-	if Unpack("") != "" {
-		t.Errorf("Expected {%s} but got {%s} ", "", Unpack(""))
+	s, err = UnpackingString("45")
+
+	if s != "" && err != nil {
+		t.Errorf("Expected {%s} but got {%s} ", expected, s)
 	}
 
-	if Unpack("45") != "" {
-		t.Errorf("Expected {%s} but got {%s} ", expected, Unpack(""))
+	s, err = UnpackingString("abcd")
+
+	if s != "abcd" && err != nil {
+		t.Errorf("Expected {%s} but got {%s} ", expected, s)
+	}
+	s, err = UnpackingString("pop2opo6k3t3mnmn6")
+
+	if s != "poppopooooookkktttmnmnnnnnn" && err != nil {
+		t.Errorf("Expected {%s} but got {%s} ", expected, s)
 	}
 
-	if Unpack("abcd") != "abcd" {
-		t.Errorf("Expected {%s} but got {%s} ", expected, Unpack(""))
-	}
-
-	if Unpack("pop2opo6k3t3mnmn6") != "poppopooooookkktttmnmnnnnnn" {
-		t.Errorf("Expected {%s} but got {%s} ", expected, Unpack(""))
-	}
 }
