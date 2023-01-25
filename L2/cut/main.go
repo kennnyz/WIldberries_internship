7558f50e05affcc8a3857300708e713e9bd9367a
Muhammed Muhtarov <muhammed1410@yandex.ru>
2023-01-25 18:01:24 +0300

    Cut

--- L2/cut/main.go
+++ L2/cut/main.go
@@ -9,29 +9,24 @@ import (
 )
 
 func main() {
-	flagf := flag.Int("f", 0, " - выбрать поля")
-	flagd := flag.String("d", "\t", "использовать другой разделитель")
+	fields := flag.Int("f", 0, " - выбрать поля")
+	delim := flag.String("d", "\t", "использовать другой разделитель")
 	flags := flag.Bool("s", false, "только строки с разделителем ")
 	flag.Parse()
 
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		input, _ := reader.ReadString('\n')
-
-		delimLine := []string{}
-		if strings.Contains(input, *flagd) {
-			delimLine = strings.Split(input, *flagd)
+		if *flags && !strings.Contains(input, *delim) {
+			continue
 		}
-
-		if *flagf > 0 {
-			fmt.Printf("You working with f flag and here is your %d field ---> %s", *flagf, delimLine[*flagf-1])
-		}
-
-		if *flags {
-			fmt.Println("You working with s flag and here is your string ---> ", input)
+		delimLine := strings.Split(input, *delim)
+		if *fields > 0 {
+			fmt.Printf("You working with f flag and here is your %d field ---> %s", *fields, delimLine[*fields-1])
+		} else if *fields > len(delimLine) {
+			fmt.Println("Field number is more than number of columns")
 		}
 	}
-
 }
 
 //
